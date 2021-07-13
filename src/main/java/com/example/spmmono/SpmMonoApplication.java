911693e4f210806476101a8cc2d179340779c26e
Guilherme dos Santos <gdossantos970@gmail.com>
2021-07-12 22:04:27 -0300

    initial commit

---
+++ src/main/java/com/example/spmmono/SpmMonoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spmmono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpmMonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpmMonoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spmmono/SpmMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spmmono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpmMonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
